34a2bd9be72941942f93817ba23f88aae04860b9
Luis <luisribeiro.lr@outlook.com>
2026-02-03 19:09:00 -0300

    Aulas de projeto

--- src/pages/Briefing.jsx
+++ src/pages/Briefing.jsx
@@ -8,7 +8,7 @@ function Briefing() {
 
     console.log('Fetching briefing content');
     // Supondo que o arquivo esteja em public/posts/meu-texto.md e isso
-    fetch(`${import.meta.env.BASE_URL}arquivos/briefing-sbm.md`)
+    fetch(`${import.meta.env.BASE_URL}arquivos/aulas-de-projeto.md`)
       .then((res) => res.text())
       .then((text) => setContent(text));
   }, []);

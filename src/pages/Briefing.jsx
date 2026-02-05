01de881a96882bfebfbe38b833ff5f190e1649f7
Luis <luisribeiro.lr@outlook.com>
2026-02-05 20:10:19 -0300

    Novo layout e briefing

--- src/pages/Briefing.jsx
+++ src/pages/Briefing.jsx
@@ -9,7 +9,7 @@ function Briefing() {
 
     console.log('Fetching briefing content');
     // Supondo que o arquivo esteja em public/posts/meu-texto.md e isso
-    fetch(`${import.meta.env.BASE_URL}arquivos/briefing-moburb.md`)
+    fetch(`${import.meta.env.BASE_URL}arquivos/briefing-moburb2.md`)
       .then((res) => res.text())
       .then((text) => setContent(text));
   }, []);
